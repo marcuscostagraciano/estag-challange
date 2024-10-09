33cc7597c8485b794b431ff3c4285aab84579968
root <mgraciano@softexpert.com>
2024-10-09 14:34:06 -0300

    Revert "REFACTOR(DatabaseConnection): add singleton functionalities"
    This reverts commit 27c404049e20a29be3820cbd9319be412c78f5e8.

--- back/src/classes/DatabaseConnection.php
+++ back/src/classes/DatabaseConnection.php
@@ -3,33 +3,31 @@
 class DatabaseConnection
 {
     private static string $HOSTNAME, $DATABASE, $USER, $PASSWORD;
-    private static PDO $conn;
 
-    private static function setConnectionCredentials(): void
+    private static function initialize(): void
     {
-        self::$HOSTNAME = 'pgsql_desafio';
-        self::$DATABASE = 'applicationphp';
-        self::$USER = 'root';
-        self::$PASSWORD = 'root';
+        if (!isset(self::$HOSTNAME)) {
+            self::$HOSTNAME = 'pgsql_desafio';
+            self::$DATABASE = 'applicationphp';
+            self::$USER = 'root';
+            self::$PASSWORD = 'root';
+        }
     }
 
-    public static function getConnection(): PDO
+    public static function getConnection(): ?object
     {
-        if (!isset(self::$conn))
-            try {
-                self::setConnectionCredentials();
-                self::$conn = new PDO(
-                    'pgsql:host=' . self::$HOSTNAME . ';dbname=' . self::$DATABASE,
-                    self::$USER,
-                    self::$PASSWORD
-                );
-
-                self::$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            } catch (Exception $e) {
-                echo "Error: " . $e->getMessage();
-                return null;
-            }
-
-        return self::$conn;
+        self::initialize();
+        try {
+            $conn = new PDO(
+                'pgsql:host=' . self::$HOSTNAME . ';dbname=' . self::$DATABASE,
+                self::$USER,
+                self::$PASSWORD
+            );
+            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            return $conn;
+        } catch (Exception $e) {
+            echo "Error: " . $e->getMessage();
+            return null;
+        }
     }
 }
