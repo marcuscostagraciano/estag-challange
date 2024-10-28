aa1d0ea1678fb103a288809286fc58a1de56952b
root <mgraciano@softexpert.com>
2024-10-28 10:55:55 -0300

    FEAT: add better 'builder' cases

--- front/src/features/products/productsSlice.js
+++ front/src/features/products/productsSlice.js
@@ -34,13 +34,29 @@ const productsSlice = createSlice({
                 state.error = action.error.message;
             })
             // Delete
+            .addCase(asyncDeleteProduct.pending, (state, action) => {
+                state.status = THUNK_STATUS.LOADING;
+            })
+            .addCase(asyncDeleteProduct.rejected, (state, action) => {
+                state.status = THUNK_STATUS.IDLE;
+                state.error = action.payload;
+            })
             .addCase(asyncDeleteProduct.fulfilled, (state, action) => {
                 state.products = state.products.filter(
                     (product) => product.code !== action.payload
                 );
             })
             // Post
+            .addCase(asyncPostProduct.pending, (state, action) => {
+                state.status = THUNK_STATUS.LOADING;
+            })
+            .addCase(asyncPostProduct.rejected, (state, action) => {
+                state.status = THUNK_STATUS.FAILED;
+                state.error = action.payload;
+            })
             .addCase(asyncPostProduct.fulfilled, (state, action) => {
+                state.error = null;
+                state.status = THUNK_STATUS.SUCCEDDED;
                 state.products.push(action.payload);
             });
     },
