0ffcda986ed1d25267a1cba9ef9e7b73e782ffc2
root <mgraciano@softexpert.com>
2024-10-28 13:46:31 -0300

    FEAT: add cart slice

--- front/src/app/store.js
+++ front/src/app/store.js
@@ -4,9 +4,11 @@ import categoriesReducer from '../features/categories/categoriesSlice';
 import productsReducer from '../features/products/productsSlice';
 import orderItemReducer from '../features/orderItem/orderItemSlice';
 import ordersReducer from '../features/orders/ordersSlice';
+import cartReducer from '../features/cart/cartSlice';
 
 export const store = configureStore({
     reducer: {
+        cart: cartReducer,
         categories: categoriesReducer,
         products: productsReducer,
         orderItem: orderItemReducer,
---
+++ front/src/features/cart/cartSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const sliceName = "cart";
+const initialState = {
+    products: [],
+    tax: 0,
+    total: 0,
+};
+
+const cartSlice = createSlice({
+    name: sliceName,
+    initialState,
+
+    reducers: {},
+});
+
+// Selectors
+const selectAllProducts = state => state.cart.products;
+const selectTax = state => state.cart.tax;
+const selectTotalValue = state => state.cart.total;
+
+export default cartSlice.reducer;
+export {
+    // Selectors
+    selectAllProducts,
+};
