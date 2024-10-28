ce37c537c7d309c0854235b165407e9acdabc970
root <mgraciano@softexpert.com>
2024-10-28 11:34:51 -0300

    FEAT: import 'orderItem' and 'orders' reducers

--- front/src/app/store.js
+++ front/src/app/store.js
@@ -2,10 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 
 import categoriesReducer from '../features/categories/categoriesSlice';
 import productsReducer from '../features/products/productsSlice';
+import orderItemReducer from '../features/orderItem/orderItemSlice';
+import ordersReducer from '../features/orders/ordersSlice';
 
 export const store = configureStore({
     reducer: {
         categories: categoriesReducer,
         products: productsReducer,
+        orderItem: orderItemReducer,
+        orders: ordersReducer,
     }
 });
