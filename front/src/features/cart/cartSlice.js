e0a856f3eb6c90c1bddc887566e4d768a6b9e89c
root <mgraciano@softexpert.com>
2024-10-31 09:57:17 -0300

    FEAT: add cart reducers

--- front/src/features/cart/cartSlice.js
+++ front/src/features/cart/cartSlice.js
@@ -5,24 +5,52 @@ const initialState = {
     products: [],
     tax: 0,
     total: 0,
+    lastRemovedProduct: {}
 };
 
 const cartSlice = createSlice({
     name: sliceName,
     initialState,
 
-    reducers: {},
+    reducers: {
+        addProduct (state, payload) {
+            const product = payload.payload.product;
+
+            const taxPayed = (product.total * payload.payload.tax) / 100;
+            state.tax += taxPayed;
+            state.total += product.total + taxPayed;
+            state.products.push({ ...product, taxPayed });
+        },
+        removeProduct (state, payload) {
+            const positionToBeRemoved = payload.payload;
+            const lastRemovedProduct = state.products.splice(positionToBeRemoved, 1)[0];
+            const totalPayed = lastRemovedProduct.total + lastRemovedProduct.taxPayed;
+
+            state.lastRemovedProduct = lastRemovedProduct;
+            state.tax -= lastRemovedProduct.taxPayed;
+            state.total -= totalPayed;
+        },
+        clearCart (state) {
+            state.products = [];
+            state.tax = 0;
+            state.total = 0;
+            state.lastRemovedProduct = {};
+        },
+    },
 });
 
 // Selectors
-const selectAllProducts = state => state.cart.products;
-const selectTax = state => state.cart.tax;
-const selectTotalValue = state => state.cart.total;
+const selectAllProductsFromCart = (state) => state.cart.products;
+const selectTax = (state) => state.cart.tax;
+const selectTotalValue = (state) => state.cart.total;
+const selectLastRemovedProduct = (state) => state.cart.lastRemovedProduct ?? {};
 
+export const { addProduct, removeProduct, clearCart } = cartSlice.actions;
 export default cartSlice.reducer;
 export {
     // Selectors
-    selectAllProducts,
+    selectAllProductsFromCart,
     selectTax,
-    selectTotalValue
+    selectTotalValue,
+    selectLastRemovedProduct,
 };
