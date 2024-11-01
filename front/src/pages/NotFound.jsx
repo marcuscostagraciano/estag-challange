d0fe988f485f02d84f927eddaac5b62aa0cb919c
root <mgraciano@softexpert.com>
2024-11-01 17:30:45 -0300

    STYLE: update message style

--- front/src/pages/NotFound.jsx
+++ front/src/pages/NotFound.jsx
@@ -1,18 +1,26 @@
+const articleStyle = {
+	gridColumn: "1/-1",
+	margin: "10vh auto",
+	fontSize: "1.5rem",
+};
+
 const notFoundMessageStyle = {
 	fontWeight: "normal",
 };
 
 function NotFound() {
 	return (
-		<>
-			<h1>Hello there, <i>adventurer</i>!</h1>
+		<article style={articleStyle}>
+			<h1>
+				Hello there, <i>adventurer</i>!
+			</h1>
 
 			<h3 style={notFoundMessageStyle}>
 				Looks like the requested page does not exist.
 				<br />
 				Please, return to an existing page!
 			</h3>
-		</>
+		</article>
 	);
 }
 
