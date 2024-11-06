d0f328891ee690b5aeeac5b73d370cc91de4b98d
root <mgraciano@softexpert.com>
2024-11-06 15:31:55 -0300

    FEAT: add artists filter

---
+++ front/src/Customer/components/SideBar/SideBar.jsx
@@ -0,0 +1,69 @@
+import { useRef } from "react";
+import "./SideBar.css";
+
+const SideBar = ({
+	artists,
+	categoriesList,
+	setFilteredProductList,
+	productsList,
+}) => {
+	const DEFAULT_SELECT = "default";
+	const categoryFilterRef = useRef();
+	const artistRef = useRef();
+
+	const handleOnChange = () => {
+		const selectedCategoryCode = categoryFilterRef.current.value;
+		const selectedArtist = artistRef.current.value;
+		const isCategoryCodeNumber = !!Number(selectedCategoryCode);
+		let filteredList = productsList;
+
+		if (isCategoryCodeNumber)
+			filteredList = filteredList.filter(
+				(product) => product.category.code == selectedCategoryCode
+			);
+		if (selectedArtist != DEFAULT_SELECT)
+			filteredList = filteredList.filter(
+				(product) => product.artist == selectedArtist
+			);
+		setFilteredProductList(filteredList);
+	};
+
+	const handleReset = () => {
+		artistRef.current.value = DEFAULT_SELECT;
+		categoryFilterRef.current.value = DEFAULT_SELECT;
+		setFilteredProductList(productsList);
+	};
+
+	return (
+		<aside className="sidebar">
+			<h1>Filtros</h1>
+			<select
+				defaultValue={DEFAULT_SELECT}
+				onChange={handleOnChange}
+				ref={categoryFilterRef}
+			>
+				<option value={DEFAULT_SELECT}>All</option>
+				{categoriesList.map((cat) => (
+					<option value={cat.code} key={cat.code}>
+						{cat.name}
+					</option>
+				))}
+			</select>
+			<select
+				defaultValue={DEFAULT_SELECT}
+				onChange={handleOnChange}
+				ref={artistRef}
+			>
+				<option value={DEFAULT_SELECT}>All</option>
+				{artists.map((artist, index) => (
+					<option value={artist} key={index}>
+						{artist}
+					</option>
+				))}
+			</select>
+			<button onClick={handleReset}>Reset</button>
+		</aside>
+	);
+};
+
+export default SideBar;
--- front/src/Customer/pages/Main/Main.jsx
+++ front/src/Customer/pages/Main/Main.jsx
@@ -4,6 +4,7 @@ import { useDispatch, useSelector } from "react-redux";
 import { THUNK_STATUS } from "../../../utils/constants";
 
 import "./Main.css";
+import Footer from "../../components/Footer/Footer";
 
 import {
 	asyncFetchProducts,
@@ -17,6 +18,7 @@ import {
 } from "../../../features/categories/categoriesSlice";
 
 import ProductCard from "../../components/ProductCard/ProductCard";
+import SideBar from "../../components/SideBar/SideBar";
 
 const Main = () => {
 	const dispatch = useDispatch();
@@ -27,58 +29,39 @@ const Main = () => {
 	const productsStatus = useSelector(getProductsStatus);
 
 	const [filteredProductList, setFilteredProductList] = useState([]);
-	const categoryFilterRef = useRef();
+	const [artists, setArtists] = useState([]);
 
 	useEffect(() => {
 		if (categoriesStatus === THUNK_STATUS.IDLE)
 			dispatch(asyncFetchCategories());
 		if (productsStatus === THUNK_STATUS.IDLE)
 			dispatch(asyncFetchProducts());
-		if (productsList) setFilteredProductList(productsList);
+		if (productsList) {
+			setFilteredProductList(productsList);
+
+			const artistsSet = new Set(
+				productsList.map((product) => product.artist)
+			);
+			setArtists([...artistsSet].sort());
+		}
 	}, [categoriesStatus, productsStatus, productsList, dispatch]);
 
 	const renderProductList = filteredProductList.map((product, index) => (
 		<ProductCard product={product} key={index} />
 	));
 
-	const handleOnChange = () => {
-		const selectedCategoryCode = categoryFilterRef.current.value;
-		const isCategoryCodeNumber = !!Number(selectedCategoryCode)
-		
-		const filteredList = productsList.filter(
-			(product) => product.category.code == selectedCategoryCode
-		);
-		setFilteredProductList(isCategoryCodeNumber ? filteredList : productsList);
-	};
-
-	const handleReset = () => {
-		categoryFilterRef.current.value = "default";
-		setFilteredProductList(productsList);
-	};
-
 	return (
 		<>
 			<main>
-				<aside className="sidebar">
-					<h1>Filtros</h1>
-					<select
-						defaultValue="default"
-						onChange={handleOnChange}
-						ref={categoryFilterRef}
-					>
-						<option value="default">All</option>
-						{categoriesList.map((cat) => (
-							<option value={cat.code} key={cat.code}>
-								{cat.name}
-							</option>
-						))}
-					</select>
-					<br />
-					<button onClick={handleReset}>Reset</button>
-				</aside>
-
+				<SideBar
+					artists={artists}
+					categoriesList={categoriesList}
+					productsList={productsList}
+					setFilteredProductList={setFilteredProductList}
+				/>
 				<section className="products-list">{renderProductList}</section>
 			</main>
+			<Footer />
 		</>
 	);
 };
