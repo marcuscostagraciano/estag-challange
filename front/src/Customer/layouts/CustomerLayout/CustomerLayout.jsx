e1ae0e88c95b06ebc82415c573aad3e09c3e9a8e
root <mgraciano@softexpert.com>
2024-11-07 14:59:40 -0300

    STYLE: add styles
    (i forgor)

--- front/src/Customer/layouts/CustomerLayout/CustomerLayout.jsx
+++ front/src/Customer/layouts/CustomerLayout/CustomerLayout.jsx
@@ -1,12 +1,21 @@
 import { Outlet } from "react-router-dom";
 
-import '../../assets/base.css'
-import './CustomerLayout.css'
+import "../../assets/base.css";
+import "./CustomerLayout.css";
 
 const CustomerLayout = (props) => {
 	return (
 		<>
-			<header>CustomerLayout</header>
+			<header id="store-header">
+				<span className="offside-regular" id="store-name">
+					SwiftStore
+				</span>
+				<span id="cart-button">
+					<button className="bi bi-cart-fill" id="see-cart-button">
+						<span id="number-products-cart">5</span>
+					</button>
+				</span>
+			</header>
 			<Outlet />
 		</>
 	);
