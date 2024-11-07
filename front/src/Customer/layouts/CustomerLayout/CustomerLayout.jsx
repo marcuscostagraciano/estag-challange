444588164db47e26ce3b404f37795f5610317728
root <mgraciano@softexpert.com>
2024-11-07 15:16:06 -0300

    FEAT: add 'cartProducts' length to button

--- front/src/Customer/layouts/CustomerLayout/CustomerLayout.jsx
+++ front/src/Customer/layouts/CustomerLayout/CustomerLayout.jsx
@@ -3,7 +3,12 @@ import { Outlet } from "react-router-dom";
 import "../../assets/base.css";
 import "./CustomerLayout.css";
 
+import { selectAllProductsFromCart } from "../../../features/cart/cartSlice";
+import { useSelector } from "react-redux";
+
 const CustomerLayout = (props) => {
+	const cartProducts = useSelector(selectAllProductsFromCart);
+
 	return (
 		<>
 			<header id="store-header">
@@ -11,8 +16,14 @@ const CustomerLayout = (props) => {
 					SwiftStore
 				</span>
 				<span id="cart-button">
-					<button className="bi bi-cart-fill" id="see-cart-button">
-						<span id="number-products-cart">5</span>
+					<button
+						className="bi bi-cart-fill"
+						id="see-cart-button"
+						onClick={() => {
+							console.log("clicked");
+						}}
+					>
+						<span id="number-products-cart">{cartProducts.length}</span>
 					</button>
 				</span>
 			</header>
