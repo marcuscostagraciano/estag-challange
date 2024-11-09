8cfe5a309bf9d7bbc40b0d8ee0052608c10a653d
Marcus Costa Graciano <marcusviniciusgraciano04@gmail.com>
2024-11-08 21:53:15 -0300

    FEAT: fetch from API if still hadn't

--- front/src/Customer/pages/ProductPage/ProductPage.jsx
+++ front/src/Customer/pages/ProductPage/ProductPage.jsx
@@ -1,22 +1,35 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useLoaderData } from "react-router-dom";
-import { useSelector } from "react-redux";
 
 import "./ProductPage.css";
 
-import { selectAllProducts } from "../../../features/products/productsSlice";
+import {
+	asyncFetchProducts,
+	selectAllProducts,
+	getProductsStatus,
+} from "../../../features/products/productsSlice";
+import { THUNK_STATUS } from "../../../utils/constants";
 
 const ProductPage = () => {
+	const dispatch = useDispatch();
+
 	const { productID } = useLoaderData();
+	const productsStatus = useSelector(getProductsStatus);
 	const selectedProduct = [...useSelector(selectAllProducts)].find(
 		(product) => product.code == productID
 	);
 
+	useEffect(() => {
+		if (productsStatus == THUNK_STATUS.IDLE) dispatch(asyncFetchProducts());
+	}, [productsStatus]);
+
 	return (
 		<>
 			<img
-				src={selectedProduct.img_url}
-				alt={"Image of '" + selectedProduct.name + "' album"}
-				id=""
+				src={selectedProduct?.img_url ?? ""}
+				alt={"Image of '" + selectedProduct?.name + "' album"}
+				id="product-image"
 			/>
 			ProductPage
 		</>
